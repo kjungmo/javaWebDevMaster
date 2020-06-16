724d22223342325f7ea5ed58d629dbde288b491d
kjungmo <63198352+kjungmo@users.noreply.github.com>
2020-06-16 17:51:29 +0900

    minor changes

--- Chapter2/src/variable/DoubleTest.java
+++ Chapter2/src/variable/DoubleTest.java
@@ -3,7 +3,14 @@ package variable;
 public class DoubleTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		double dNum = 3.14;
+		
+		float fNum = 3.14f;
+		
+		System.out.println(dNum);
+		System.out.println(fNum);
+		
 
 	}
 
--- Chapter2/src/variable/DoubleTest2.java
+++ Chapter2/src/variable/DoubleTest2.java
@@ -3,7 +3,7 @@ package variable;
 public class DoubleTest2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
