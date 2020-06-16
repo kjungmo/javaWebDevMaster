76558b9dbdd7dcc02e4ed17f536d3885d420d0f7
kjungmo <63198352+kjungmo@users.noreply.github.com>
2020-06-16 23:01:45 +0900

    조건연산문

--- Chapter4/src/ifexample/IfExample2.java
+++ Chapter4/src/ifexample/IfExample2.java
@@ -6,30 +6,28 @@ public class IfExample2 {
 
 	public static void main(String[] args) {
 		
-		Scanner scanner = new Scanner(System.in);
+		/*
+		 * Scanner scanner = new Scanner(System.in);
+		 * 
+		 * int score = scanner.nextInt();
+		 * 
+		 * char grade;
+		 * 
+		 * if( score <= 59 ) { grade = 'F'; } else if( score < 70 ) { grade ='D'; } else
+		 * if( score < 80) { grade = 'C'; } else if( score <90){ grade = 'B'; } else {
+		 * grade = 'A'; }
+		 * 
+		 * System.out.println("score : " + score ); System.out.println("grade : " +
+		 * grade );
+		 */
 		
-		int score = scanner.nextInt();
+		int a = 10;
+		int b = 20;
 		
-		char grade;
+		int max;
 		
-		if( score <= 59 ) {
-			grade = 'F';
-		}
-		else if( score < 70 ) {
-			grade ='D';
-		}
-		else if( score < 80) {
-			grade = 'C';
-		}
-		else if( score <90){
-			grade = 'B';
-		}
-		else {
-			grade = 'A';
-		}
-		
-		System.out.println("score : " + score );
-		System.out.println("grade : " + grade );
+		max = (a > b)? a: b;
+		System.out.println(max);
 	}
 
 }
