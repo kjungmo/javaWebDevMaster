db6052419f6f389ea4cc1ef7d033f6086df19a78
kjungmo <63198352+kjungmo@users.noreply.github.com>
2020-06-16 16:55:37 +0900

    ~04

--- Chapter2/src/variable/VariableTest.java
+++ Chapter2/src/variable/VariableTest.java
@@ -3,8 +3,15 @@ package variable;
 public class VariableTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int age, count;
+		age = 10;
+		
+		//age = 30;
+		
+		System.out.println(age);
 
+		int numberOfStudent; // camel Notation, camel code
+		
 	}
 
 }
