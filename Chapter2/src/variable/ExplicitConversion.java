31baa0cdebdf0d3dd1271ec7a43189f1ab83083f
kjungmo <63198352+kjungmo@users.noreply.github.com>
2020-06-16 20:48:20 +0900

    implicit & explicit Conversion

---
+++ Chapter2/src/variable/ExplicitConversion.java
@@ -0,0 +1,13 @@
+package variable;
+
+public class ExplicitConversion {
+
+	public static void main(String[] args) {
+
+		int i = 1000;
+		byte bNum = (byte)i;
+		System.out.println(bNum); //데이터 유실 있을 수 있다 명시적 타입 변환은
+		
+	}
+
+}
