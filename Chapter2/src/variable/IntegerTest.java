1d1d23738039d28fbf813434e2d3f59b51c108d9
kjungmo <63198352+kjungmo@users.noreply.github.com>
2020-06-16 17:17:49 +0900

    ~06

---
+++ Chapter2/src/variable/IntegerTest.java
@@ -0,0 +1,16 @@
+package variable;
+
+public class IntegerTest {
+
+	public static void main(String[] args) {
+		
+		byte bs = -128;
+		
+		System.out.println(bs);
+		
+		//int ival = 12345678900;
+		long lval = 12345678900L;
+
+	}
+
+}
