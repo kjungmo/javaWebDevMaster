34fb1468d0560dd3cadbad96ec9e765da1607cb3
kjungmo <63198352+kjungmo@users.noreply.github.com>
2020-06-16 17:53:08 +0900

    float, double의 오차부분 관련

--- Chapter2/src/variable/DoubleTest2.java
+++ Chapter2/src/variable/DoubleTest2.java
@@ -4,7 +4,14 @@ public class DoubleTest2 {
 
 	public static void main(String[] args) {
 		
+		double dNum = 1;
+		
+		for( int i = 0; i < 10000; i++) {
+			
+			dNum = dNum + 0.1;
+		}
 
+		System.out.println(dNum);  //부동소수점 오차 부분에 대해 알 수 있다. 
 	}
 
 }
