4b1a19d5b4c746625f9f063493efe7860f7587ac
kjungmo <63198352+kjungmo@users.noreply.github.com>
2020-06-16 20:43:19 +0900

    boolean

--- Chapter2/src/variable/BooleanTest.java
+++ Chapter2/src/variable/BooleanTest.java
@@ -8,6 +8,13 @@ public class BooleanTest {
 		
 		System.out.println(isMarried);
 
+		var i = 10;
+		var i2 = 10.1;
+		var i3 = "df";
+		
+		System.out.println(i);
+		System.out.println(i2);
+		System.out.println(i3);
 	}
 
 }
