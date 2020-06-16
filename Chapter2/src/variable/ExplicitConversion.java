4250077eb9578c4aa382eef23cbb54fbc6cf4852
kjungmo <63198352+kjungmo@users.noreply.github.com>
2020-06-16 20:55:32 +0900

    type conversion example2

--- Chapter2/src/variable/ExplicitConversion.java
+++ Chapter2/src/variable/ExplicitConversion.java
@@ -8,6 +8,14 @@ public class ExplicitConversion {
 		byte bNum = (byte)i;
 		System.out.println(bNum); //데이터 유실 있을 수 있다 명시적 타입 변환은
 		
+		double dNum1 = 1.2;
+		float fNum = 0.9F;
+		
+		int iNum1 = (int)dNum1 + (int)fNum;
+		int iNum2 = (int)(dNum1 + fNum);
+		
+		System.out.println(iNum1);
+		System.out.println(iNum2);
 	}
 
 }
