21cd6b6089f00a3e477d9ab17d62fdbebfd1b643
kjungmo <63198352+kjungmo@users.noreply.github.com>
2020-06-16 22:05:39 +0900

    단락회로평가

--- Chapter3/src/operator/OperatorEx3.java
+++ Chapter3/src/operator/OperatorEx3.java
@@ -12,6 +12,22 @@ public class OperatorEx3 {
 		System.out.println(--score);
 		System.out.println(score--);
 		System.out.println(score);
+		
+		int num1 = 10;
+		int i = 2;
+		
+		boolean value = ((num1 = num1 + 10) < 10) && ((i = i + 2) < 10);
+		System.out.println(num1);
+		System.out.println(i);
+		
+		System.out.println(value); // 앞이 false 나와서 뒤 항은 연산 안됐다
+		
+		boolean value2 = ((num1 = num1 + 10) < 10) || ((i = i + 2) < 10);
+		System.out.println(num1);  //OR연산자이기 때문에 뒤 연산됐다.
+		System.out.println(i);
+		
+		System.out.println(value2);
+		
 	}
 
 }
