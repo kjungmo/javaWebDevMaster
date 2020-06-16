4b70d294dbeed3019438cdff10d35dffbb1deddb
kjungmo <63198352+kjungmo@users.noreply.github.com>
2020-06-16 17:30:59 +0900

    char

--- Chapter2/src/variable/CharTest.java
+++ Chapter2/src/variable/CharTest.java
@@ -10,7 +10,13 @@ public class CharTest {
 		System.out.println((int)ch); //type casting
 		
 		int iCh = 66;
+		System.out.println(iCh);
+		System.out.println((char)iCh);
 		
+		//char ch2 = -66; // 음수값은 들어갈 수 없다.
+		
+		char hangul = '\uAC00';
+		System.out.println(hangul);
 	}
 
 }
