aa37d95c4960150a88d3d19cd8e9ff85581cea48
kjungmo <63198352+kjungmo@users.noreply.github.com>
2020-06-16 21:28:35 +0900

    operator example 2

--- Chapter3/src/operator/OperatorEx2.java
+++ Chapter3/src/operator/OperatorEx2.java
@@ -4,21 +4,11 @@ public class OperatorEx2 {
 
 	public static void main(String[] args) {
 
-		int num1 = -10;
-		int num2 = 20;
-		
-		System.out.println(+num1);
-		System.out.println(+num2);
-		
-		System.out.println(-num1);
-		System.out.println(-num2);
-		
-		System.out.println(num1);
+		int num = 5%3;
+		System.out.println(num);
+		int num2 = 10%3;
 		System.out.println(num2);
-		
-		num1 = -num1;
-		
-		System.out.println(num1);
+	
 	}
 
 }
