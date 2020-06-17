223b0920e23de88ef904959970b28e8d100205b2
kjungmo <63198352+kjungmo@users.noreply.github.com>
2020-06-17 14:43:41 +0900

    while 에 scanner 적용

--- Chapter4/src/loopexample/WhileExample.java
+++ Chapter4/src/loopexample/WhileExample.java
@@ -1,18 +1,29 @@
 package loopexample;
 
+import java.util.Scanner;
+
 public class WhileExample {
 
 	public static void main(String[] args) {
 
-		int num = 1;
-		int sum = 0;
+		/*
+		 * int num = 1; int sum = 0;
+		 * 
+		 * while(num <=10) { sum += num; num++; } System.out.println(sum);
+		 * System.out.println(num);
+		 */
 		
-		while(num <=10) {
-			sum += num;
-			num++;
+		int input;
+		int sum = 0;
+		Scanner scanner = new Scanner(System.in);
+		input = scanner.nextInt();
+		
+		while(input !=0) {
+			sum +=input;
+			input = scanner.nextInt();
 		}
+		
 		System.out.println(sum);
-		System.out.println(num);
 	}
 
 }
