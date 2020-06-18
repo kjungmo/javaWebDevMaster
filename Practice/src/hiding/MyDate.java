f9a4c8b457d38c9637afe9b36fb8cd053f6d0d91
kjungmo <63198352+kjungmo@users.noreply.github.com>
2020-06-18 18:12:48 +0900

    public

---
+++ Practice/src/hiding/MyDate.java
@@ -0,0 +1,12 @@
+package hiding;
+
+public class MyDate {
+
+	int day;
+	int month;
+	int year;
+	
+	public void showDate() {
+		System.out.println( year + "³â " + month + "¿ù " + day + "ÀÏ");
+	}
+}
