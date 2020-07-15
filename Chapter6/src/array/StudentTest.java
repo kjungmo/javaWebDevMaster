47e3ad8db3b87b31529979febf13623955ffe4b9
kjungmo <63198352+kjungmo@users.noreply.github.com>
2020-07-15 23:12:56 +0900

    chapter6 student

--- Chapter6/src/array/StudentTest.java
+++ Chapter6/src/array/StudentTest.java
@@ -1,22 +1,23 @@
-/*
- * package array;
- * 
- * public class StudentTest {
- * 
- * public static void main(String[] args) {
- * 
- * Student studentLee = new Student(1001, "Lee");
- * 
- * studentLee.addSubject("국어", 100); studentLee.addSubject("영어", 90);
- * 
- * Student studentKim = new Student(1002, "Kim"); studentKim.addSubject("국어",
- * 100); studentKim.addSubject("영어", 90); studentKim.addSubject("수학", 80);
- * 
- * studentLee.showStudentInfo();
- * System.out.println("====================================");
- * studentKim.showStudentInfo();
- * 
- * }
- * 
- * }
- */
+package array;
+  
+  	public class StudentTest {
+  
+  		public static void main(String[] args) {
+  
+  			Student studentLee = new Student(1001, "Lee");
+   			studentLee.addSubject("국어", 100); 
+  			studentLee.addSubject("영어", 90);
+  
+  			Student studentKim = new Student(1002, "Kim"); 
+  			studentKim.addSubject("국어", 100); 
+  			studentKim.addSubject("영어", 90); 
+  			studentKim.addSubject("수학", 80);
+  
+  			studentLee.showStudentInfo();
+  			System.out.println("====================================");
+  			studentKim.showStudentInfo();
+  
+  		}
+  
+  }
+ 
